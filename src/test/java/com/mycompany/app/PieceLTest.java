b5821efc6ff32142e952244888778729a4f169b0
NoeDiez <noeliatatianadiez@icloud.com>
2025-09-18 18:13:44 -0300

    pieza L

--- src/test/java/com/mycompany/app/PieceLTest.java
+++ src/test/java/com/mycompany/app/PieceLTest.java
@@ -1,10 +1,71 @@
 package com.mycompany.app;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.Test;
 
 public class PieceLTest {
-    
+
+    @Test
+    public void creaPiezaL() {
+        int filas = 3;
+        int columnas = 2;
+
+        int matriz[][] = new int[filas][columnas]; 
+
+        for (int i = 0; i < filas; i++) { 
+            for (int j = 0; j < columnas; j++) {
+                assertEquals(0, matriz[i][j]);
+            }
+        }
+    }
+
+    @Test
+    public void asignaValoresL() {
+        String matriz[][] = new String[3][2]; 
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                matriz[i][j] = ".";
+            }
+        }
+
+        //  L derecha 
+        matriz[0][1] = "*";
+        matriz[1][1] = "*";
+        matriz[2][1] = "*";
+        matriz[2][0] = "*";
+
+        assertEquals("*", matriz[0][1]);
+        assertEquals("*", matriz[1][1]);
+        assertEquals("*", matriz[2][1]);
+        assertEquals("*", matriz[2][0]);
+
+        assertEquals(".", matriz[0][0]);
+        assertEquals(".", matriz[1][0]);
+    }
+
+    @Test
+    public void asignaValoresLIzquierda() {
+        String matriz[][] = new String[3][2];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                matriz[i][j] = ".";
+            }
+        }
+
+        // L izq
+        matriz[0][0] = "*";
+        matriz[1][0] = "*";
+        matriz[2][0] = "*";
+        matriz[2][1] = "*";
+
+        assertEquals("*", matriz[0][0]);
+        assertEquals("*", matriz[1][0]);
+        assertEquals("*", matriz[2][0]);
+        assertEquals("*", matriz[2][1]);
+
+        assertEquals(".", matriz[0][1]);
+        assertEquals(".", matriz[1][1]);
+    }
 }
--- src/test/java/com/mycompany/app/PieceStickTest.java
+++ src/test/java/com/mycompany/app/PieceStickTest.java
@@ -10,30 +10,30 @@ public class PieceStickTest {
         int filas = 4;
         int columnas = 1;
 
-        int matriz[][] = new int[filas][columnas];
+    int matriz[][] = new int[filas][columnas]; // crea una matriz de 4 filas y 1 columna
 
-        for (int i = 0; i < filas; i++) {
+        for (int i = 0; i < filas; i++) { // inicializa la matriz con 0
             for (int j = 0; j < columnas; j++) {
                 assertEquals(0, matriz[i][j]);
-            }
+            } 
         }
     }
 
     @Test
     public void asignaValoresStick() {
-        String matriz[][] = new String[4][1];
+        String matriz[][] = new String[4][1]; // crea una matriz de 4 filas y 1 columna
 
       
-        for (int i = 0; i < 4; i++) {
-            matriz[i][0] = ".";
+        for (int i = 0; i < 4; i++) { // inicializa la matriz con "."
+            matriz[i][0] = "."; 
         }
 
-        matriz[0][0] = "*";
+        matriz[0][0] = "*"; // asigna los valores para formar la pieza "Stick"
         matriz[1][0] = "*";
         matriz[2][0] = "*";
         matriz[3][0] = "*";
 
-        assertEquals("*", matriz[0][0]);
+        assertEquals("*", matriz[0][0]); // comprueba que los valores se asignaron correctamente
         assertEquals("*", matriz[1][0]);
         assertEquals("*", matriz[2][0]);
         assertEquals("*", matriz[3][0]);
