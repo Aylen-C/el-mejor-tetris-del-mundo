ff2fdc1cf118e820aca9c4f66d15eee74ed0079d
Matildaaz717 <matildaazcona108@gmail.com>
2025-09-18 00:29:05 +0000

    Colocar piezas en el tablero

--- src/test/java/com/mycompany/app/BoardTest.java
+++ src/test/java/com/mycompany/app/BoardTest.java
@@ -1,6 +1,7 @@
 package com.mycompany.app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +10,8 @@ public class BoardTest {
 
  @Test   
  public void CreaTablero () {
-    int filas = 10 ;
-    int columnas = 20; 
+    int filas = 20 ;  //cambiar a 20 
+    int columnas = 10; // cambiar a 10 
 
     int matriz [][]= new int [filas][columnas];
 
@@ -21,18 +22,54 @@ public class BoardTest {
         }
     }
  }
+
     @Test
     public void PiezasTalbero() {
-         Board board = new BoardTest(); 
 
-         int [][] PieceT(){
+         Board board = new Board(20,10); 
 
-         return new int[][] {
-            {8,4} ,{8,5}, {8,6}, {9,5}
-         };  
-         }
+         int [][] PieceSqueare= {
+            {19,3} ,{19,4}, {18,3}, {18,4}  
+         };
+
+         boolean ColocarPieza = board.Colocar(PieceSqueare);
+         assertTrue(ColocarPieza, "La pieza esta dentro del tablero");
+
+        assertEquals(0,board.Colocar(19,3));
+        assertEquals(0, board.Colocar(19,4));
+        assertEquals(0, board.Colocar(18,3));
+        assertEquals(0, board.Colocar(18,4));
+
+
+        int [][] PieceStick= {
+            {19,6} ,{18,6}, {17,6}, {16,6}  
+         };
+
+         ColocarPieza = board.Colocar(PieceStick); //no es necesario volver a poner boolean 
+         assertTrue(ColocarPieza, "La pieza esta dentro del tablero");
+
+        assertEquals(0, board.Colocar(19,6));
+        assertEquals(0, board.Colocar(18,6));
+        assertEquals(0, board.Colocar(17,6));
+        assertEquals(0, board.Colocar(16,6));
+
     } 
+
+    @Test
+    public void FueraDelTablero(){
+
+        
+
+
+    }
+
+
+
+
+
     //parametro de interfaces
+
+    
 
 
 
