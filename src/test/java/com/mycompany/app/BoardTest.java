2e4270f3d109e14427f0500d58175eb3e80ef4c2
Matildaaz717 <matildaazcona108@gmail.com>
2025-09-18 20:34:00 +0000

    Merge branch 'main' of https://github.com/Aylen-C/el-mejor-tetris-del-mundo

--- src/test/java/com/mycompany/app/BoardTest.java
+++ src/test/java/com/mycompany/app/BoardTest.java
@@ -8,23 +8,23 @@ import org.junit.jupiter.api.Test;
 
 public class BoardTest {
 
+ @Test   
+ public void CreaTablero () {
+    int filas = 20 ;  //cambiar a 20 
+    int columnas = 10; // cambiar a 10 
 
-   @Test   
-   public void CreaTablero () {
-      int filas = 20 ;  //cambiar a 20 
-      int columnas = 10; // cambiar a 10 
+    int matriz [][]= new int [filas][columnas];
 
-      int matriz [][]= new int [filas][columnas];
-      for (int i = 0; i<filas; i++){
+    for (int i = 0; i<filas; i++){
         for (int j = 0; j<columnas; j++){
 
             assertEquals(0, matriz[i][j]);
-         }
-      }
-   }
+        }
+    }
+ }
 
     @Test
-    public void PiezasTablero() {
+    public void PiezasTalbero() {
 
          Board board = new Board(20,10); 
 
@@ -35,10 +35,10 @@ public class BoardTest {
          boolean ColocarPieza = board.Colocar(PieceSqueare);
          assertTrue(ColocarPieza, "La pieza esta dentro del tablero");
 
-        assertEquals(1, board.Colocar(19,3));
-        assertEquals(1, board.Colocar(19,4));
-        assertEquals(1, board.Colocar(18,3));
-        assertEquals(1, board.Colocar(18,4));
+        assertEquals(0, board.Colocar(19,3));
+        assertEquals(0, board.Colocar(19,4));
+        assertEquals(0, board.Colocar(18,3));
+        assertEquals(0, board.Colocar(18,4));
 
 
         int [][] PieceStick= {
@@ -48,35 +48,50 @@ public class BoardTest {
          ColocarPieza = board.Colocar(PieceStick); //no es necesario volver a poner boolean 
          assertTrue(ColocarPieza, "La pieza esta dentro del tablero");
 
-        assertEquals(1, board.Colocar(19,6));
-        assertEquals(1, board.Colocar(18,6));
-        assertEquals(1, board.Colocar(17,6));
-        assertEquals(1, board.Colocar(16,6));
-      } 
+        assertEquals(0, board.Colocar(19,6));
+        assertEquals(0, board.Colocar(18,6));
+        assertEquals(0, board.Colocar(17,6));
+        assertEquals(0, board.Colocar(16,6));
+
+    } 
 
     @Test
     public void FueraDelTablero(){
-    Board board = new Board(20, 10);
-         int [][] pieceSquareAfuera= {
+      
+      Board board = new Board(20,10);
+
+         int [][] PieceSqueare= {
             {20,3} ,{20,4}, 
             {18,3}, {18,4}  
          };
-         boolean EstaAfuera = board.Afuera(pieceSquareAfuera);
-         assertTrue(EstaAfuera, "Hay piezas que estan fuera del tablero");
 
-        int [][] pieceStickAfuera= {
+         boolean EstaAfuera = board.Afuera(PieceSqueare);
+         assertFalse(EstaAfuera, "La pieza no puede estar fuera del tablero");
+
+
+
+        int [][] PieceStick= {
             {21,6} ,
             {20,6}, 
             {19,6},
             {17,6}  
          };
 
-         EstaAfuera = board.Afuera(pieceStickAfuera); //no es necesario volver a poner boolean 
+         EstaAfuera = board.Afuera(PieceStick); //no es necesario volver a poner boolean 
          assertTrue(EstaAfuera, "La pieza no puede estar fuera del tablero");
 
-   }
+    }
+
+
+
 
 
     //parametro de interfaces
 
+    
+
+
+
+    
+ 
 }
