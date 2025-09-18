195910c03128f5cdef61c3c7205ed301438bfc87
NoeDiez <noeliatatianadiez@icloud.com>
2025-09-18 18:36:38 -0300

    rotacion dog y stick

--- src/main/java/com/mycompany/app/PieceDog.java
+++ src/main/java/com/mycompany/app/PieceDog.java
@@ -1,5 +1,39 @@
 package com.mycompany.app;
-public class PieceDog extends Piece{
+public class PieceDog extends Piece {
 
+    @Override
+    public String[][] forma() {
 
+        switch (posicionActual) {
+
+            case 0: //dog derecha
+                return new String[][] {
+                    {".", "*", "*"},
+                    {"*", "*", "."},
+                    {".", ".", "."},
+                };
+            case 1: //dog derecha (rotado)
+                return new String[][] {
+                    {"*", ".", "."},
+                    {"*", "*", "."},
+                    {".", "*", "."},
+                };
+            case 2: //dog izquierda
+                return new String[][] {
+                    {"*", "*", "."},
+                    {".", "*", "*"},
+                    {".", ".", "."},
+                };
+            case 3: //dog izquierda (rotado)
+                return new String[][] {
+                    {".", "*", "."},
+                    {"*", "*", "."},
+                    {"*", ".", "."},
+                };
+        }
+        return null;
+     }
+         
 }
+
+
--- src/main/java/com/mycompany/app/PieceStick.java
+++ src/main/java/com/mycompany/app/PieceStick.java
@@ -1,4 +1,23 @@
 package com.mycompany.app;
 
-public class PieceStick {
+public class PieceStick extends Piece {
+
+
+@Override
+public String[][] forma() {
+    switch (posicionActual) {
+        case 0: //stick vertical
+        return new String[][] {
+            {"*"},
+            {"*"},
+            {"*"},
+            {"*"}
+        };
+        case 1: //stick horizontal
+        return new String[][] {
+            {"*", "*", "*", "*"}
+        };
+    }
+    return null;
+}
 }
