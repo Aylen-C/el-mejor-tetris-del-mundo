0e291879a78a8cdcc416d9e6fde2e3cd2ebb1fed
Aylen <aylenagostina2006@gmail.com>
2025-09-19 01:44:12 +0000

    .

--- src/main/java/com/mycompany/app/Board.java
+++ src/main/java/com/mycompany/app/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private int columnas;
     private int [][] board; 
 
+    public boolean moveDown() {
+        // mueve la pieza hacia abajo
+    }
     
     public Board(int filas, int columnas) {
         this.filas = filas;
