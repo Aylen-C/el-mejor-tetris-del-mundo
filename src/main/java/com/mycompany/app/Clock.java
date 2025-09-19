b08adf8899efb003309f3dee5b4848ca49a7b7c8
Matildaaz717 <matildaazcona108@gmail.com>
2025-09-19 03:07:40 +0000

    Clock inicio

--- src/main/java/com/mycompany/app/Clock.java
+++ src/main/java/com/mycompany/app/Clock.java
@@ -1,6 +1,13 @@
 package com.mycompany.app;
 
 public class Clock {
+    private int tick;
+    private int tickContador;  
+
+    public Clock() {
+        this.tick = 2;
+        this.tickContador = 0;
+    }
  
     
     
