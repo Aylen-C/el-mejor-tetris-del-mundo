7874edaba96899725ad7683bb95fd953e832e63a
Aylen <aylenagostina2006@gmail.com>
2025-09-22 17:24:15 +0000

    Dog2 Test

--- src/test/java/com/mycompany/app/PieceDog2Test.java
+++ src/test/java/com/mycompany/app/PieceDog2Test.java
@@ -1,67 +1,33 @@
 package com.mycompany.app;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PieceDog2Test {
 
     @Test
-    public void formaHorizontal() {
+    public void formaNormal() {
         PieceDog2 pieza = new PieceDog2();
         pieza.setPosicionActual(0);
         int[][] esperado = {
-            {1, 1, 0},
-            {0, 1, 1},
-            {0, 0, 0}
-        };
-        int[][] resultado = pieza.formaNumerica();
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                assertEquals(esperado[i][j], resultado[i][j]);
+            {0,0}, {0,1},
+                   {1,1}, {1,2}    
+       };
+        int[][] resultado = pieza.forma();
+        assertArrayEquals(esperado, resultado);
     }
 
     @Test
-    public void formaVertical() {
+    public void formaDerecha() {
         PieceDog2 pieza = new PieceDog2();
         pieza.setPosicionActual(1);
         int[][] esperado = {
-            {0, 1, 0},
-            {1, 1, 0},
-            {1, 0, 0}
+                {0,1},
+        {1,0}, {1,1},
+        {2,0}
         };
-        int[][] resultado = pieza.formaNumerica();
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                assertEquals(esperado[i][j], resultado[i][j]);
-    }
-
-    @Test
-    public void formaHorizontalInvertida() {
-        PieceDog2 pieza = new PieceDog2();
-        pieza.setPosicionActual(2);
-        int[][] esperado = {
-            {0, 0, 0},
-            {1, 1, 0},
-            {0, 1, 1}
-        };
-        int[][] resultado = pieza.formaNumerica();
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                assertEquals(esperado[i][j], resultado[i][j]);
-    }
-
-    @Test
-    public void formaVerticalInvertida() {
-        PieceDog2 pieza = new PieceDog2();
-        pieza.setPosicionActual(3);
-        int[][] esperado = { 
-            {1, 0, 0},
-            {1, 1, 0},
-            {0, 1, 0}
-        };
-        int[][] resultado = pieza.formaNumerica();
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                assertEquals(esperado[i][j], resultado[i][j]);
+        int[][] resultado = pieza.forma();
+        assertArrayEquals(esperado, resultado);
     }
 }
