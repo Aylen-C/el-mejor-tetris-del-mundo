05aa9579a8c315b22d61e2e2202c6ec3d447e354
Matildaaz717 <matildaazcona108@gmail.com>
2025-09-22 17:09:38 +0000

    test tick

--- src/test/java/com/mycompany/app/ClockTest.java
+++ src/test/java/com/mycompany/app/ClockTest.java
@@ -2,17 +2,29 @@ package com.mycompany.app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;;
 
 public class ClockTest {
+
+   @Test
+    public void BajaCompleto(){
     Board board = new Board(20, 10);
     Clock clock = new Clock();
+    PieceSquare square = new PieceSquare();
+    board.setPiece(square, 0, 4); // Coloca la pieza en la parte superior del tablero
 
-    public void AbajaCompleto(){
+    for(int tick=0; tick<4; tick++){
+        boolean puedebajar = board.moveDown();
+        assertEquals(true, puedebajar);
+
 
     }
 
-    
+
 
     
+}
 }
