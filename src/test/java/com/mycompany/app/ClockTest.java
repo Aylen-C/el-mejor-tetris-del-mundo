ec0946c9980b3045d10b8f69b81120c164f5e809
Matildaaz717 <matildaazcona108@gmail.com>
2025-09-22 21:13:22 +0000

    test clock 2

--- src/test/java/com/mycompany/app/ClockTest.java
+++ src/test/java/com/mycompany/app/ClockTest.java
@@ -10,19 +10,53 @@ import static org.junit.jupiter.api.Assertions.assertFalse;;
 public class ClockTest {
 
    @Test
-    public void BajaCompleto(){
+    public void Baja2Filas(){
     Board board = new Board(20, 10);
     Clock clock = new Clock();
     PieceSquare square = new PieceSquare();
-    board.setPiece(square, 0, 4); // Coloca la pieza en la parte superior del tablero
+    board.setPiezaActual(square.forma(), 4); // Coloca la pieza en la parte superior del tablero
+
+
+    int bajadas=0; 
 
     for(int tick=0; tick<4; tick++){
+        if (clock.tick()){
         boolean puedebajar = board.moveDown();
-        assertEquals(true, puedebajar);
+        assertTrue(puedebajar,"Puede seguir bajando");
+        bajadas++;
 
-
+        }
     }
 
+    assertEquals(2,bajadas);
+    
+}
+
+
+@Test
+public void BajaCompleto(){
+    Board board = new Board(20, 10);
+    Clock clock = new Clock();
+    PieceSquare square = new PieceSquare();
+    board.setPiezaActual(square.forma(), 4);
+
+    int bajadas=0; 
+
+    for(int tick=0; tick<38; tick++){
+        if (clock.tick()){
+        boolean puedebajar = board.moveDown();
+        }
+        if (!puedebajar()){
+            break;
+        }
+        bajadas++;
+    }
+
+    
+assertTrue(puedebajar,"Puede seguir bajando");
+    assertEquals(19,bajadas);
+    assertFalse(puedebajar,"No puede seguir bajando");
+    
 
 
     
