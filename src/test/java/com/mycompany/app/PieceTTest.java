93e450f1ca7bd21fe72f3022ae1e1b0b2e000c86
Matildaaz717 <matildaazcona108@gmail.com>
2025-09-26 06:07:35 +0000

    arreglo de test pieza T

--- src/test/java/com/mycompany/app/PieceTTest.java
+++ src/test/java/com/mycompany/app/PieceTTest.java
@@ -55,38 +55,3 @@ public class PieceTTest {
         assertArrayEquals(esperado, resultado);
     }
 }
-
-
-
-    /*
-     @Test
-     public void AsignaValores (){
-        
-        String matriz [][] = new String [2][3];
-
-        // INICIALIZA
-        for (int i = 0; i < 2; i++){
-            for (int j = 0; j < 3; j++){
-                matriz[i][j] = ".";
-            }
-        }
-
-        // T
-        matriz[0][0] ="*"; 
-        matriz[0][1]= "*";
-        matriz[0][2]= "*"; 
-        matriz[1][1]= "*"; 
-        
-        // comprueba
-        assertEquals("*", matriz[0][0]);
-        assertEquals("*", matriz[0][1]);
-        assertEquals("*", matriz[0][2]);
-        assertEquals("*", matriz[1][1]);
-        
-        // relleno
-        assertEquals(".", matriz[1][0]);
-        assertEquals(".", matriz[1][2]);
-
-    }
-}
- */
--- src/test/java/com/mycompany/app/PieceTest.java
+++ src/test/java/com/mycompany/app/PieceTest.java
@@ -5,49 +5,5 @@ import org.junit.jupiter.api.Test;
 
 public class PieceTest {
 
-    @Test
-    public void testPosicionInicial() {
-        Piece pieza = new PieceL(); 
-        assertEquals(0, pieza.getPosicionActual());
-    }
-
-    @Test
-    public void testRotateRight() {
-        Piece pieza = new PieceL();
-        pieza.rotateRight();
-        assertEquals(1, pieza.getPosicionActual());
-    }
-
-    @Test
-    public void testRotateLeft() {
-        Piece pieza = new PieceL();
-        pieza.rotateLeft();
-        assertEquals(3, pieza.getPosicionActual());
-    }
-        @Test
-    public void noPuedeDescenderCuandoHayObstaculo() {
-        Piece pieza = new PieceL();
-        pieza.setPosicionActual(0);
-        String[][] tablero = new String[5][5];
-        for (int i = 0; i < 5; i++)
-            for (int j = 0; j < 5; j++)
-                tablero[i][j] = ".";
-        tablero[3][1] = "*";
-        tablero[3][0] = "*";
-        boolean puedeBajar = pieza.puedeDescender(tablero, 1, 0);
-        assertEquals(false, puedeBajar);
-    }
-
-    @Test
-    public void noPuedeRotarSiChocaConOtraPieza() {
-        Piece pieza = new PieceL();
-        pieza.setPosicionActual(0);
-        String[][] tablero = new String[5][5];
-        for (int i = 0; i < 5; i++)
-            for (int j = 0; j < 5; j++)
-                tablero[i][j] = ".";
-        tablero[2][1] = "*";
-        boolean puedeRotar = pieza.puedeRotar(tablero, 1, 0);
-        assertEquals(false, puedeRotar);
-    }
+    
 }
