ba87b0015bbeca9ed4b128484c9fdc713dbf5d60
Matildaaz717 <matildaazcona108@gmail.com>
2025-09-26 05:50:35 +0000

    perder test

---
+++ src/test/java/com/mycompany/app/PerderTest.java
@@ -0,0 +1,38 @@
+package com.mycompany.app;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;;
+
+public class PerderTest {
+
+    @Test
+    public void perderPartida(){
+    Board board = new Board(20, 10);
+    Clock clock = new Clock();
+    PieceSquare square = new PieceSquare();
+
+    boolean perder = false; 
+
+    for (int caidas=0; caidas<11; caidas++){
+      board.setPiezaActual(square.forma(), 0,5);
+
+        boolean moverPieza = board.moveDown(); //intenta bajar la pieza actual una fila
+        if (!moverPieza){ //si no puede bajar la pieza, significa que llegó
+        perder = true; 
+        break; 
+      }
+     while (true) {
+        if (clock.tick()) { // si el reloj indica que es tiempo de bajar la pieza
+            boolean puedeBajar = board.moveDown();
+            if (!puedeBajar) {
+                perder= true;
+                break; // la pieza no puede bajar más, salir del bucle
+            }
+        }
+       }
+      }
+        assertTrue(perder, "Perdio la partida");
+    }
+
+    }
+    
+
