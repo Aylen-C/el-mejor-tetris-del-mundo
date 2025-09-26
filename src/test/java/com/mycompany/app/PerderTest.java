7aa795ee0ca49463cdf84ccd52aed640a17c6f25
Matildaaz717 <matildaazcona108@gmail.com>
2025-09-26 08:26:44 +0000

    no se puede agregar linea test

--- src/test/java/com/mycompany/app/PerderTest.java
+++ src/test/java/com/mycompany/app/PerderTest.java
@@ -1,6 +1,7 @@
 package com.mycompany.app;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PerderTest {
 
@@ -33,6 +34,32 @@ public class PerderTest {
         assertTrue(perder, "Perdio la partida");
     }
 
+
+    @Test
+    public void noPuedeIngresarPieza(){
+      Board board = new Board(20, 10);
+      PieceSquare square = new PieceSquare();
+      Clock clock = new Clock();
+
+      for (int caidas=0; caidas<11; caidas++){
+      board.setPiezaActual(square.forma(), 0,5); 
+
+      boolean puedeIngresar = true;
+     while (true) {
+        if (clock.tick()) { // si el reloj indica que es tiempo de bajar la pieza
+            puedeIngresar = board.moveDown();
+            if (!puedeIngresar) {
+                break; // la pieza no puede bajar más, salir del bucle
+            }
+        }
+       }
+      }
+
+      boolean pudoColocar = board.setPiezaActual(square.forma(), -1, 5);
+      // Verificar que no se pudo ingresar la pieza
+      assertFalse(pudoColocar, "No se puede ingresar la pieza.");
     }
+}
+
     
 
