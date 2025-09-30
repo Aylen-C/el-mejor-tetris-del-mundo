15ada0cb2b9627ff6f5f896b7be6e1cc93c07e73
Aylen <aylenagostina2006@gmail.com>
2025-09-30 07:39:06 +0000

    PieceBase

--- src/main/java/com/mycompany/app/PieceBase.java
+++ src/main/java/com/mycompany/app/PieceBase.java
@@ -4,7 +4,7 @@ public abstract class PieceBase implements IRotator {
 
     public static PieceBase nuevaPieza() {
         int cantidad = 7; // cantidad de piezas
-        int tipo= (int) (Math.random() * cantidad);
+        int tipo = (int) (Math.random() * cantidad);
         switch (tipo) {
             case 0: return new PieceSquare();
             case 1: return new PieceL();
@@ -19,53 +19,44 @@ public abstract class PieceBase implements IRotator {
 
     private int posicionActual = 0;
 
-    int getPosicionActual(){
+    int getPosicionActual() {
         return posicionActual;
     }
 
-    void setPosicionActual(int nuevaPosicion){
+    void setPosicionActual(int nuevaPosicion) {
         posicionActual = nuevaPosicion;
     }
 
-    int orientacionActual(){
+    int orientacionActual() {
         return posicionActual;
     }
-    
-    public void rotateRight(){
-        switch (posicionActual){//  0 = normal   -   1 = derecha   -   2 = abajo   -   3 = izquierda
 
-            case 0: posicionActual = 1;   // normal -> derecha
-                break;
-
-            case 1: posicionActual = 2;   // derecha -> abajo
-                break;
-
-            case 2: posicionActual = 3;   // abajo -> izquierda
-                break;
-
-            case 3: posicionActual = 0;   // izquierda -> normal
-                break;
+    public void rotateRight() {
+        switch (posicionActual) { //  0 = normal   -   1 = derecha   -   2 = abajo   -   3 = izquierda
+            case 0: posicionActual = 1;
+            break; // normal -> derecha
+            case 1: posicionActual = 2;
+            break; // derecha -> abajo
+            case 2: posicionActual = 3;
+            break; // abajo -> izquierda
+            case 3: posicionActual = 0;
+            break; // izquierda -> normal
         }
     }
-    
 
-    public void rotateLeft(){
-        switch (posicionActual){
-
-            case 0: posicionActual = 3;   // normal -> izquierda
-                break;
-
-            case 1: posicionActual = 0;   // derecha -> normal
-                break;
-
-            case 2: posicionActual = 1;   // abajo -> derecha
-                break;
-
-            case 3: posicionActual = 2;   // izquierda -> abajo
-                break;
+    public void rotateLeft() {
+        switch (posicionActual) {
+            case 0: posicionActual = 3;
+            break; // normal -> izquierda
+            case 1: posicionActual = 0;
+            break; // derecha -> normal
+            case 2: posicionActual = 1;
+            break; // abajo -> derecha
+            case 3: posicionActual = 2;
+            break; // izquierda -> abajo
         }
     }
-    
+
     public abstract int[][] forma();  /* cada pieza implementa su propia forma y  
                                         la devuelve segun su posicion actual
                                         (0, 1, 2, 3)  */
