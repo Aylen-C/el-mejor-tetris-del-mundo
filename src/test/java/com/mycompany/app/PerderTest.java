1d7e3be76b4c390d1fa62332de2ae47b191993d5
Matildaaz717 <matildaazcona108@gmail.com>
2025-09-30 03:21:43 +0000

    Actualización en Board y PerderTest

--- src/test/java/com/mycompany/app/PerderTest.java
+++ src/test/java/com/mycompany/app/PerderTest.java
@@ -55,7 +55,7 @@ public class PerderTest {
        }
       }
 
-      boolean pudoColocar = board.setPiezaActualSobreescribir(square.forma(), -1, 5);
+      boolean pudoColocar = board.intentaColocar(square.forma(), -1, 5);
       // Verificar que no se pudo ingresar la pieza
       assertFalse(pudoColocar, "No se puede ingresar la pieza, no hay mas lugar.");
     }
