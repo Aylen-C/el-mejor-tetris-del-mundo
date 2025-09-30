607e7beaa580a47610c8b63eccf2ff290f692679
Aylen <aylenagostina2006@gmail.com>
2025-09-30 04:49:54 +0000

    .

--- src/main/java/com/mycompany/app/PieceSquare.java
+++ src/main/java/com/mycompany/app/PieceSquare.java
@@ -18,29 +18,4 @@ public class PieceSquare extends Piece {
         };
     }
 
-    @Override
-    public boolean puedeDescender(String[][] tablero, int fila, int columna) {
-        int[][] formaActual = forma();
-        for (int[] coord : formaActual) {
-            int nuevaFila = fila + coord[0] + 1;
-            int nuevaColumna = columna + coord[1];
-            if (nuevaFila >= tablero.length || !tablero[nuevaFila][nuevaColumna].equals(".")) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public boolean puedeRotar(String[][] tablero, int fila, int columna) {
-        int[][] formaActual = forma();
-        for (int[] coord : formaActual) {
-            int f = fila + coord[0];
-            int c = columna + coord[1];
-            if (f < 0 || f >= tablero.length || c < 0 || c >= tablero[0].length || !tablero[f][c].equals(".")) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
